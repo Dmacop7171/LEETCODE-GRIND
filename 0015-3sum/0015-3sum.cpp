9d7d0e87d83e45c4cf9c64ab9080d6fe4be7abb1
David <115887252+Dmacop7171@users.noreply.github.com>
2026-08-27 14:04:09 +0530

    Time: 55 ms (26.83%), Space: 31.1 MB (16.17%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> s;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) 
-                continue; // Skip duplicate elements
-            
-            int j = i + 1;
-            int k = nums.size() - 1;
-            
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    s.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-                    k--;
+        vector<vector<int>> ans;
+        sort(nums.begin() , nums.end());
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1 , k = nums.size()-1;
 
-                    // Skip duplicate elements for `j` and `k`
-                    while (j < k && nums[j] == nums[j - 1]) 
-                        j++;
-                    while (j < k && nums[k] == nums[k + 1]) 
-                        k--;
+            while(j < k){
+                int target = nums[i] + nums[j] + nums[k];
+
+                if(target == 0){
+                    vector<int> temp = {nums[i] , nums[j] , nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
                 }
+                else if( target < 0) j++;
+                else k--;
             }
         }
-        return s;
+
+        return ans;
     }
 };
